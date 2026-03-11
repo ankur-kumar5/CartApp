34b47d2d496c2bf49042b6c8c7ac4761b025c317
ankur-kumar5 <134390248+ankur-kumar5@users.noreply.github.com>
2026-03-11 23:58:47 +0530

    build Title changed

--- src/components/counter.js
+++ src/components/counter.js
@@ -20,7 +20,7 @@ const Counter = () => {
 
     return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Redux Toolkit Counter</h1>
+      <h1>Redux Toolkit Counter Cart App</h1>
       <h2>{count}</h2>
 
       <button onClick={() => counterHandler("increment")}>+</button>
